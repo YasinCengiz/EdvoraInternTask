6dae445b8ef52bd2cdde90d09e9d1c67adcceba8
Yasin <mryasincengiz@gmail.com>
2022-03-01 13:22:59 +0400

    Displays JSONData in a list

--- Edvora/ContentView.swift
+++ Edvora/ContentView.swift
@@ -14,33 +14,39 @@ struct ContentView: View {
     var body: some View {
         List(rideModel, id: \.id) { item in
             VStack {
-//                Text(item.id)
-//                Text(String(item.originStationCode))
-//                Text(String(item.stationPath))
-//                Text(String(item.date))
+                Text("ID: \(item.id)")
+                Text("Origin Station Code: \(item.origin_station_code)")
+                ForEach(item.station_path, id:\.self) {i in
+                    Text("Station Path: \(i)")
+                }
+                Text("Destination Station Code: \(item.destination_station_code)")
+                Text("Date: \(item.date)")
+                Text("Map URL: \(item.map_url)")
+                Text("State: \(item.state)")
+                Text("City: \(item.city)")
             }
         }
         .task {
-//            await loadData()
+            await loadData()
         }
     }
     
-//    func loadData() async {
-//        guard let url = URL(string: "https://assessment.api.vweb.app/rides") else {
-//            print("URL ERROR: INVALID URL")
-//            return
-//        }
-//        do {
-//            // ignoring metadata for now
-//            let (data, _) = try await URLSession.shared.data(from: url)
-//            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-//                rideModel = decodedResponse.results
-//            }
-//        } catch {
-//            print("DATA ERROR: \(error)")
-//        }
-//    }
-//
+    func loadData() async {
+        guard let url = URL(string: "https://assessment.api.vweb.app/rides") else {
+            print("URL ERROR: INVALID URL")
+            return
+        }
+        do {
+            // ignoring metadata for now
+            let (data, _) = try await URLSession.shared.data(from: url)
+            if let decodedResponse = try? JSONDecoder().decode([RideModel].self, from: data) {
+                rideModel = decodedResponse
+            }
+        } catch {
+            print("DATA ERROR: \(error)")
+        }
+    }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
