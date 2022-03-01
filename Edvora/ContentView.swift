424cf7cf79f25c75f99659cfa4ebce987a4b2d3e
Yasin <mryasincengiz@gmail.com>
2022-03-01 11:51:27 +0400

    Initial Commit

---
+++ Edvora/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Edvora
+//
+//  Created by Yasin on 1.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Edvora/EdvoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdvoraApp.swift
+//  Edvora
+//
+//  Created by Yasin on 1.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct EdvoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
